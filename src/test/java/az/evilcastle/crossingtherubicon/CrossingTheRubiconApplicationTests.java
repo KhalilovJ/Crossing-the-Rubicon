9009acb47ab75f8ef363f3d047cf272169e8cb15
malishov <alishov.murad831@gmail.com>
2024-05-12 13:21:54 +0400

    CR-15: Add Skeleton

---
+++ src/test/java/az/evilcastle/crossingtherubicon/CrossingTheRubiconApplicationTests.java
@@ -0,0 +1,13 @@
+package az.evilcastle.crossingtherubicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossingTheRubiconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
