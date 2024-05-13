50600ab30838e36d2c87b2054f8cfb53bb480a08
KhalilovJ <24898110+KhalilovJ@users.noreply.github.com>
2024-05-13 23:47:11 +0400

    Comment post construct

--- src/main/java/az/evilcastle/crossingtherubicon/test/TestService.java
+++ src/main/java/az/evilcastle/crossingtherubicon/test/TestService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class TestService {
     private final TestRedisRepo testRedisRepo;
 
-    @PostConstruct
+//    @PostConstruct
     private void testRedis(){
         System.out.println(testRedisRepo.save(TestEntity.builder().string("str").build()));
     }
