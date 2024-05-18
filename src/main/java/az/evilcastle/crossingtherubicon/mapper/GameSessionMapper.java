fa2ddfb352b21d8b7af0f81d9536a79bae9fbedc
KhalilovJ <24898110+KhalilovJ@users.noreply.github.com>
2024-05-18 12:49:33 +0400

    Clean build comment

--- src/main/java/az/evilcastle/crossingtherubicon/mapper/GameSessionMapper.java
+++ src/main/java/az/evilcastle/crossingtherubicon/mapper/GameSessionMapper.java
@@ -5,6 +5,9 @@ import az.evilcastle.crossingtherubicon.model.dto.GameSessionDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
+/**
+ *  Perform Clean Build when editing old mapper
+ */
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface GameSessionMapper {
 
